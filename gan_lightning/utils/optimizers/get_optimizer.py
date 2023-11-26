650756ff4a3cfca7b3fafb57d2ae3e11a601c371
umutdundar99 <umutdundar37@gmail.com>
2023-11-26 12:39:12 +0300

    optimizer is updated accord. new config strctr

--- gan_lightning/utils/optimizers/get_optimizer.py
+++ gan_lightning/utils/optimizers/get_optimizer.py
@@ -20,27 +20,29 @@ __optimizers__ = [
 __schedulers__ = [StepLR]
 
 
-def get_optimizer(model_params, optimizer_config: str, **kwargs):
-    optimizer_dict = OmegaConf.to_container(optimizer_config)
+def get_optimizer(model_params, optimizer_dict: str, **kwargs):
     scheduler_dict = optimizer_dict.get("scheduler", None)
-    if optimizer_config.name not in [optimizer.__name__.lower() for optimizer in __optimizers__]:
-        raise ValueError(f"Optimizer {optimizer_config.name} not found")
+    if optimizer_dict["optimizer_name"] not in [optimizer.__name__.lower() for optimizer in __optimizers__]:
+        name = optimizer_dict["optimizer_name"]
+        raise ValueError(f"Optimizer {name} not found")
 
     for optimizer in __optimizers__:
-        if optimizer.__name__.lower() == optimizer_config.name.lower():
+        if optimizer.__name__.lower() == optimizer_dict["optimizer_name"].lower():
             optimizer = optimizer
             break
-    logging.info("Using optimizer: {}".format(optimizer_config.name))
+    logging.info("Using optimizer: {}".format(optimizer_dict["optimizer_name"].capitalize()))
 
     if scheduler_dict is None:
         logging.info("No scheduler found")
         return optimizer(lr=optimizer_dict["lr"], **kwargs)
     else:
-        if scheduler_dict["name"] not in [scheduler.__name__ for scheduler in __schedulers__]:
-            raise ValueError("Scheduler {} not found".format(scheduler_dict["name"]))
+        if scheduler_dict["scheduler_name"] not in [scheduler.__name__ for scheduler in __schedulers__]:
+            raise ValueError("Scheduler {} not found".format(scheduler_dict["scheduler_name"]))
         for scheduler in __schedulers__:
-            if scheduler.__name__.lower() == scheduler_dict["name"].lower():
+            if scheduler.__name__.lower() == scheduler_dict["scheduler_name"].lower():
                 scheduler = scheduler
                 break
-        logging.info(f"Using scheduler: {scheduler_dict['name']}")
-        return scheduler(optimizer(model_params, lr=optimizer_dict["lr"], **kwargs), **scheduler_dict["params"])
+        logging.info(f"Using scheduler: {scheduler_dict['scheduler_name']}")
+        optimizer = optimizer(params=model_params, lr=optimizer_dict["lr"], **kwargs)
+
+        return optimizer, scheduler(optimizer, step_size=scheduler_dict["step_size"], gamma=scheduler_dict["gamma"])
