d2a0c5cdc87d78599a60ab95de0fb100528fe3e0
umutdundar99 <umutdundar37@gmail.com>
2023-12-07 20:03:18 +0300

    Refactor generator and discriminator classes

--- gan_lightning/src/models/discriminator/simple_discriminator.py
+++ gan_lightning/src/models/discriminator/simple_discriminator.py
@@ -2,7 +2,6 @@ from torch import nn
 from gan_lightning.src.models.blocks.discriminator_block.discriminator_blocks import (  # noqa
     simple_1d_discriminator_block,
 )
-from gan_lightning.utils.optimizers.get_optimizer import get_optimizer
 import lightning.pytorch as pl
 
 from gan_lightning.src.models import model_registration
@@ -12,7 +11,6 @@ from gan_lightning.src.models import model_registration
 class Simple_Discriminator(pl.LightningModule):
     def __init__(self, img_channel: int = 784, hidden_dim=128, **kwargs):
         super().__init__()
-        self.kwargs = kwargs
         self.discriminator = nn.Sequential(
             simple_1d_discriminator_block(img_channel, hidden_dim * 4),
             simple_1d_discriminator_block(hidden_dim * 4, hidden_dim * 2),
