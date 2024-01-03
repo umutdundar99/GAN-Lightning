785972b3c5193172893a8ac85c4f900cbf70bbdb
umutdundar99 <umutdundar37@gmail.com>
2024-01-03 23:13:17 +0300

    datamodule functionality is added to pipeline

--- gan_lightning/input/__init__.py
+++ gan_lightning/input/__init__.py
@@ -1,11 +1,11 @@
-registered_datasets = {}
+registered_dataloaders = {}
 
 
 def register_dataset(dataset_name):
     def register(dataset_cls):
-        if dataset_name in registered_datasets["datasets"]:
+        if dataset_name in registered_dataloaders:
             raise ValueError(f"Model {dataset_name} is already registered")
-        registered_datasets["datasets"][dataset_name] = dataset_cls
+        registered_dataloaders[dataset_name] = dataset_cls
         return dataset_cls
 
     return register
---
+++ gan_lightning/input/data/__init__.py
@@ -0,0 +1,1 @@
+from gan_lightning.input.data.datamodules import *
