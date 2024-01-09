214878736d12c1e08c9f718c51294930e8d87b93
umutdundar99 <umut.dundar@smartalpha.ai>
2024-01-09 12:31:55 +0300

    Add image channel and number of classes constants

--- gan_lightning/utils/constants.py
+++ gan_lightning/utils/constants.py
@@ -2,5 +2,25 @@ from dataclasses import dataclass
 
 
 @dataclass
+class img_channel:
+    MNIST: int = 1
+    CIFAR10: int = 3
+    FashionMNIST: int = 1
+    CelebA: int = 3
+    LSUN: int = 3
+
+
+@dataclass
+class num_classes:
+    MNIST: int = 10
+    CIFAR10: int = 10
+    FashionMNIST: int = 10
+    CelebA: int = 2
+    LSUN: int = 2
+
+
+@dataclass
 class Constants:
     augment_config_dir: str = "gan_lightning/src/config/augment_config.yaml"
+    img_channel: img_channel = img_channel()
+    num_classes: num_classes = num_classes()
